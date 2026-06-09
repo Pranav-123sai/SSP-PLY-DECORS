4c27539d47b0849e712f2d2fb477f248404c751b
Pranav A <sai2004pranav@gmail.com>
2026-06-09 13:07:43 +0530

    Update products.js

--- products.js
+++ products.js
@@ -516,26 +516,3 @@ popup.addEventListener("click",(e)=>{
     }
 
 });
-
-/* ===========================
-   MOBILE MENU
-=========================== */
-
-<script>
-const hamburger = document.getElementById("hamburger");
-const mobileMenu = document.getElementById("mobileMenu");
-
-hamburger.addEventListener("click", function(e){
-    e.stopPropagation();
-    mobileMenu.classList.toggle("show");
-});
-
-document.addEventListener("click", function(){
-    mobileMenu.classList.remove("show");
-});
-
-mobileMenu.addEventListener("click", function(e){
-    e.stopPropagation();
-});
-</script>
-
